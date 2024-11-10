34877cddcc9a56e768c5d1b32964855e55446b7d
Mag1sstr <danilkarachev234@mail.ru>
2024-11-10 22:16:56 +0500

    Create Header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,40 @@
+import "./Header.css";
+import logoImage from "../../images/logo.svg";
+import searchImage from "../../images/header/search.svg";
+import likedImage from "../../images/header/liked.svg";
+import basketImage from "../../images/header/basket.svg";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <div className="conteiner">
+        <div className="header__row">
+          <img src={logoImage} alt="logo" />
+          <div className="header__user">
+            <div className="header__user-image"></div>
+            <p className="header__user-name">Unknown user</p>
+          </div>
+          <div className="header__search">
+            <img
+              className="header__search-img"
+              src={searchImage}
+              alt="search"
+            />
+            <input
+              className="header__search-input"
+              type="text"
+              placeholder="Search for anything..."
+            />
+          </div>
+          <div className="header__icons">
+            <img src={likedImage} alt="" />
+            <div className="header__basket-block">
+              <img src={basketImage} alt="" />
+              <div className="header__basket-circle">1</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
