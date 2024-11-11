93c12670d83f55655b86093184748e6c4dca46eb
Mag1sstr <danilkarachev234@mail.ru>
2024-11-11 18:28:19 +0500

    add categories section

--- src/components/CategoriesSection/CategoriesSection.jsx
+++ src/components/CategoriesSection/CategoriesSection.jsx
@@ -1,7 +1,21 @@
 import "./CategoriesSection.css";
-import computerImage from "../../images/categoriesSection/comp.png";
+
+import { Route, Routes } from "react-router-dom";
+import BigSale from "../BigSale/BigSale";
+import Basket from "../Basket/Basket";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function CategoriesSection() {
+  // API взято с сайта https://fakestoreapi.com/docs
+
+  const [categories, setCategories] = useState(null);
+  useEffect(() => {
+    axios.get("https://fakestoreapi.com/products/categories").then((resp) => {
+      setCategories(resp.data);
+    });
+  }, []);
+
   return (
     <section className="categories">
       <div className="conteiner">
@@ -9,13 +23,19 @@ export default function CategoriesSection() {
           <div className="categories__leftside">
             <h1 className="categories__leftside-title">CATEGORIES</h1>
             <div className="categories__leftside-items">
-              <p className="categories__leftside-item">Computers</p>
-              <p className="categories__leftside-item">Clothes</p>
+              {categories?.map((item, index) => {
+                return (
+                  <p key={index} className="categories__leftside-item">
+                    {item}
+                  </p>
+                );
+              })}
+              {/* <p className="categories__leftside-item">Clothes</p>
               <p className="categories__leftside-item">Shoes</p>
               <p className="categories__leftside-item">Furniture</p>
               <p className="categories__leftside-item">Cosmetics</p>
               <p className="categories__leftside-item">Travel</p>
-              <p className="categories__leftside-item">Automotive</p>
+              <p className="categories__leftside-item">Automotive</p> */}
             </div>
             <div className="categories__help">
               <p className="categories__help-text">Help</p>
@@ -24,7 +44,11 @@ export default function CategoriesSection() {
               </p>
             </div>
           </div>
-          <div className="categories__rightside">
+          <Routes>
+            <Route path="/" element={<BigSale />} />
+            <Route path="/basket" element={<Basket />} />
+          </Routes>
+          {/* <div className="categories__rightside">
             <p className="categories__absolute-text">BIG SALE 20%</p>
             <div className="categories__rightside-inner">
               <p className="categories__rightside-text">
@@ -37,7 +61,7 @@ export default function CategoriesSection() {
               <button className="categories__button">Shop Now</button>
             </div>
             <img className="categories__image" src={computerImage} alt="" />
-          </div>
+          </div> */}
         </div>
       </div>
     </section>
