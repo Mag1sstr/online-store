f4f2d2df77f3ad571ff0bec1ba3c5a6beff367c6
Mag1sstr <danilkarachev234@mail.ru>
2024-11-11 18:27:33 +0500

    add material ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import "./App.css";
 import CategoriesSection from "./components/CategoriesSection/CategoriesSection";
+import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
+import LessSection from "./components/LessSection/LessSection";
+import TrendingSection from "./components/TrendingSection/TrendingSection";
 
 function App() {
   return (
     <div className="app">
       <Header />
       <CategoriesSection />
+      <TrendingSection />
+      <LessSection />
+      <Footer />
     </div>
   );
 }
