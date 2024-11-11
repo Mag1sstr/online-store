473e0456295447d107bb551d278a3d8eba15577a
Mag1sstr <danilkarachev234@mail.ru>
2024-11-12 00:07:21 +0500

    Create AuthContext, fix Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import CategoriesSection from "./components/CategoriesSection/CategoriesSection";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import LessSection from "./components/LessSection/LessSection";
 import TrendingSection from "./components/TrendingSection/TrendingSection";
+import Main from "./components/Main/Main";
+import Basket from "./components/Basket/Basket";
+import CategoryProduct from "./components/CategoryProduct/CategoryProduct";
 
 function App() {
   return (
     <div className="app">
       <Header />
-      <CategoriesSection />
-      <TrendingSection />
-      <LessSection />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/basket" element={<Basket />} />
+        <Route path="/categories/:name" element={<CategoryProduct />} />
+      </Routes>
+
       <Footer />
     </div>
   );
---
+++ src/contexts/AuthContext.jsx
@@ -0,0 +1,58 @@
+import axios from "axios";
+import { createContext, useEffect, useState } from "react";
+
+export const AuthContext = createContext();
+
+export default function AuthContextProvider({ children }) {
+  const [user, setUser] = useState(null);
+  const [token, setToken] = useState(localStorage.getItem("token"));
+  function parseJwt(token) {
+    var base64Url = token.split(".")[1];
+    var base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+    var jsonPayload = decodeURIComponent(
+      atob(base64)
+        .split("")
+        .map(function (c) {
+          return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+        })
+        .join("")
+    );
+    return JSON.parse(jsonPayload);
+  }
+  useEffect(() => {
+    if (token) {
+      //   console.log(parseJwt(token).sub);
+
+      axios
+        .get(`https://fakestoreapi.com/users/${parseJwt(token).sub}`)
+        .then((resp) => {
+          console.log(resp.data);
+          setUser(resp.data);
+        });
+    }
+  }, [token]);
+
+  function signIn(username, password) {
+    axios
+      .post("https://fakestoreapi.com/auth/login", {
+        username: username,
+        password: password,
+      })
+      .then((resp) => {
+        axios.defaults.headers.common["Authorization"] = "Bearer " + token;
+        setToken(resp.data.token);
+        localStorage.setItem("token", resp.data.token);
+      });
+  }
+  return (
+    <AuthContext.Provider
+      value={{
+        user: user,
+        signIn: signIn,
+        setUser: setUser,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+}
